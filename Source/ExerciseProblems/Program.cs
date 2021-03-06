1abbce8f326f2f9c875c4085d482b8e1669135e7
Pranav Krishnamurthy <74801920+pkcodes92@users.noreply.github.com>
2021-03-06 15:56:53 -0500

    First code push, barebones (#1)

---
+++ Source/ExerciseProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciseProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
