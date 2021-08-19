109c0089703b48068521ee8b8cbc38fbe5066d29
pkcodes92 <pkrish19@outlook.com>
2021-08-19 19:05:59 -0400

    Making sure that the changes are done

---
+++ Source/ExerciseProblems/Exceptions/MatrixMultiplicationException.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="MatrixMultiplicationException.cs" company="PKCodes">
+// Copyright (c) PKCodes. All rights reserved.
+// </copyright>
+
+namespace ExerciseProblems.Exceptions
+{
+    /// <summary>
+    /// This exception class will be able to throw the right exceptions in the matrix multiplications.
+    /// </summary>
+    public class MatrixMultiplicationException
+    {
+    }
+}
