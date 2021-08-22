e64b40cba4a9b986410588f62d1ccf9a24121630
pkcodes92 <pkrish19@outlook.com>
2021-08-22 16:18:13 -0400

    Having the necessary things moving forward correctly, in order to have the right unit testing done

--- Source/ExerciseProblems.Tests/MatrixMultiplicationTest.cs
+++ Source/ExerciseProblems.Tests/MatrixMultiplicationTest.cs
@@ -4,6 +4,8 @@
 
 namespace ExerciseProblems.Tests
 {
+    using System;
+    using ExerciseProblems.Exceptions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     /// <summary>
@@ -12,5 +14,28 @@ namespace ExerciseProblems.Tests
     [TestClass]
     public class MatrixMultiplicationTest
     {
+        /// <summary>
+        /// This method is the standard baseline test method.
+        /// </summary>
+        [TestMethod]
+        public void MatrixMultiplication_Baseline_Test()
+        {
+            int[] first = new int[] { 1, 2, 3, 5, 6 };
+            int[] second = new int[] { 2, 3, 4, 5, 7 };
+            var baselineResult = Solver.MultiplyArrayElements(first, second, first.Length);
+            Console.WriteLine($"{baselineResult}");
+            Assert.IsNotNull(baselineResult);
+        }
+
+        /// <summary>
+        /// This test method will ensure that there is an exception thrown.
+        /// </summary>
+        [TestMethod]
+        public void MatrixMultiplication_Exception_Test()
+        {
+            int[] first = new int[] { 1, 2, 3, 4, 5 };
+            int[] second = new int[] { };
+            Assert.ThrowsException<MatrixMultiplicationException>(() => Solver.MultiplyArrayElements(first, second, first.Length));
+        }
     }
 }
