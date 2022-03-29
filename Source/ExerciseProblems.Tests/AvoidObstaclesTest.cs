5a97483664577c7a110f2ef597a5dd7d1c7523e8
pkcodes92 <pkrish19@outlook.com>
2022-03-29 13:56:53 -0400

    Having the unit testing for the Avoid Obstacles problem

---
+++ Source/ExerciseProblems.Tests/AvoidObstaclesTest.cs
@@ -0,0 +1,30 @@
+// <copyright file="AvoidObstaclesTest.cs" company="PKCodes">
+// Copyright (c) PKCodes. All rights reserved.
+// </copyright>
+
+
+namespace ExerciseProblems.Tests
+{
+    using System;
+    using ExerciseProblems;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// This class will test the functionality of the solution to the avoidObstacles problem.
+    /// </summary>
+    [TestClass]
+    public class AvoidObstaclesTest
+    {
+        /// <summary>
+        /// This method will test the functionality of <see cref="Solver.AvoidObstacles(int[])"/>.
+        /// </summary>
+        [TestMethod]
+        public void AvoidObstacles_BasicTest()
+        {
+            int[] inputArray = { 3, 5, 4, 6, 7, 8, 9 };
+            var output = Solver.AvoidObstacles(inputArray);
+            Console.WriteLine($"output = {output}");
+            Assert.IsTrue(output > 0);
+        }
+    }
+}
